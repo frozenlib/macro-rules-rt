a5d69e92370dbb2a30cc660468fbc1bca02eb06c
frozenlib <frozenlib@users.noreply.github.com>
2023-04-02 17:05:05 +0900

    Fix test.

--- tests/replace_all.rs
+++ tests/replace_all.rs
@@ -21,7 +21,7 @@ input = {input}"
         let rule = Rule::new(from, to).unwrap();
         let rule = rule.nest(nest);
         let actual = rule.replace_all(input).unwrap();
-        assert_eq!(actual, expect, "replace_all_str str {msg}");
+        assert_eq!(actual, expect, "replace_all str {msg}");
     }
     {
         let from: TokenStream = parse_str(from).unwrap();
@@ -31,12 +31,16 @@ input = {input}"
         let rule = Rule::new(from, to).unwrap();
         let rule = rule.nest(nest);
         let actual = rule.replace_all(input).unwrap();
-        assert_eq!(actual, expect, "replace_all_str tokens {msg}");
+        assert_eq!(actual, expect, "replace_all tokens {msg}");
 
         let input: TokenStream = input.parse().unwrap();
         let expect: TokenStream = expect.parse().unwrap();
         let actual = rule.replace_all_tokens(input);
-        assert_eq!(actual.to_string(), expect.to_string(), "replace_all {msg}");
+        assert_eq!(
+            actual.to_string(),
+            expect.to_string(),
+            "replace_all_tokens {msg}"
+        );
     }
 }
 
